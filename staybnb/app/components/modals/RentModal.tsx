078d8a825e0eccfeea65cd3bfba4db130ab13f9f
Wisesofthemall <foxxydieujuste@gmail.com>
2023-06-13 11:17:46 -0400

    REact leaflet map Completion

--- staybnb/app/components/modals/RentModal.tsx
+++ staybnb/app/components/modals/RentModal.tsx
@@ -6,6 +6,8 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 type Props = {};
 enum STEPS {
@@ -43,6 +45,14 @@ function RentModal({}: Props) {
   });
 
   const category = watch("category");
+  const location = watch("location");
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location],
+  );
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
       shouldValidate: true,
@@ -92,12 +102,28 @@ function RentModal({}: Props) {
       </div>
     </div>
   );
+
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8 ">
+        <Heading
+          title="Where is your place located"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
   return (
     <Modal
       title="Staybnb your home!"
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
