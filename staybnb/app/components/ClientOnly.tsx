e41995f3d92dc3a24768a10dc87230093b028965
Wisesofthemall <foxxydieujuste@gmail.com>
2023-07-17 04:11:04 -0400

    fix: Complete ClientOnly Component for rehydration errors

---
+++ staybnb/app/components/ClientOnly.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+
+type Props = {
+  childeren: React.ReactNode;
+};
+
+function ClientOnly({ childeren }: Props) {
+  const [hasMounted, setHasMounted] = useState(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
+  return <div>{childeren}</div>;
+}
+
+export default ClientOnly;
